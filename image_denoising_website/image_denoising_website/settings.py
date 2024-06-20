fb6067bf0f5fbb06ebcf00969d5239d014bffc4d
Ramesh Chandra Soren <138772294+rameshgitter@users.noreply.github.com>
2024-06-20 17:18:39 +0530

    Update settings.py

--- image_denoising_website/image_denoising_website/settings.py
+++ image_denoising_website/image_denoising_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1=k!_9n_j+f^lq0kwq$tcw6+7k2d1haoqeso43+!$2$$$944b_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
